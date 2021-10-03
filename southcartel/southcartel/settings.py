de0e8e86418c18d0c7093b89ff4742481ea4c983
Jeffrey Jacobo <jacobojeffrey1@gmail.com>
2021-10-03 16:47:41 +0800

    applied hosts

--- southcartel/southcartel/settings.py
+++ southcartel/southcartel/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['southcartel-deploy-env.eba-esrpvqfb.us-west-2.elasticbeanstalk.com',
+ALLOWED_HOSTS = ['southcartel-app-env.eba-6avvpkdw.us-west-2.elasticbeanstalk.com',
 '127.0.0.1']
 
 
