0c37e54f8bde4cf0e916c4fc2a0521069a5dd74a
Jeffrey Jacobo <jacobojeffrey1@gmail.com>
2021-10-04 14:58:05 +0800

    updated settings

--- southcartel/southcartel/settings.py
+++ southcartel/southcartel/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,13 +142,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT= BASE_DIR /'static'
+STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    'southcartel/static',
+    os.path.join(BASE_DIR, 'southcartel/static'),
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR /'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
