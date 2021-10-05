43659441c7d61a48f924b51519ae163873aafb37
Jeffrey Jacobo <jacobojeffrey1@gmail.com>
2021-10-05 12:54:08 +0800

    view changes

--- southcartel/southcartel/settings.py
+++ southcartel/southcartel/settings.py
@@ -169,6 +169,7 @@ EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 
 JAZZMIN_SETTINGS = {
     #  "site_icon": "books/img/icon.png",
