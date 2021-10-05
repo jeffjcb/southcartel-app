492d37255b1a87d5243f599377e9ddd6f36d801e
Jeffrey Jacobo <jacobojeffrey1@gmail.com>
2021-10-06 00:02:37 +0800

    add allowed hosts

--- southcartel/southcartel/settings.py
+++ southcartel/southcartel/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['southcartel-test.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['southcartel-test.herokuapp.com', '127.0.0.1', 'www.southcartel55.com', 'southcartel55.com']
 
 
 # Application definition
